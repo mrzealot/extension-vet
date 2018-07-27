3c5b9e2c0ada86f79bb132e155a46e2582398247
Bán Dénes <mr@zealot.hu>
2018-07-27 18:35:53 +0200

    Add actual set of permissions + perform comparison to manifest + add preliminary results

--- index.js
+++ index.js
@@ -9,9 +9,71 @@ const rimraf = promisify(require('rimraf'))
 const espree = require('espree')
 const estraverse = require('estraverse')
 
+
+// quick parse of the available permission strings from 
+// https://developer.chrome.com/extensions/declare_permissions
+// without sub-levels
+const permissions = [
+    'activeTab',
+    'alarms',
+    'background',
+    'bookmarks',
+    'browsingData',
+    'certificateProvider',
+    'clipboardRead',
+    'clipboardWrite',
+    'contentSettings',
+    'contextMenus',
+    'cookies',
+    'debugger',
+    'declarativeContent',
+    'declarativeNetRequest',
+    'declarativeWebRequest',
+    'desktopCapture',
+    'displaySource',
+    'dns',
+    'documentScan',
+    'downloads',
+    'experimental',
+    'fileBrowserHandler',
+    'fileSystemProvider',
+    'fontSettings',
+    'gcm',
+    'geolocation',
+    'history',
+    'identity',
+    'idle',
+    'idltest',
+    'management',
+    'nativeMessaging',
+    'notifications',
+    'pageCapture',
+    'platformKeys',
+    'power',
+    'printerProvider',
+    'privacy',
+    'processes',
+    'proxy',
+    'sessions',
+    'signedInDevices',
+    'storage',
+    'tabCapture',
+    'tabs ',
+    'topSites',
+    'tts',
+    'ttsEngine',
+    'unlimitedStorage',
+    'wallpaper',
+    'webNavigation',
+    'webRequest',
+    'webRequestBlocking',
+];
+
+
 // assume that .crx files are already scraped using the helper script/ids from the scraping folder
 
 const inputFolder = 'input' // TODO: command line params
+let result = 'id;actual;declared;union;intersection;undeclared;unused;errors\n'
 
 // shamelessly "repurposed" from StackOverflow
 function fromDir(startPath,filter,callback){
@@ -35,46 +97,48 @@ async function main() {
 
     let temp = path.resolve('tmp')
 
-    const permissions = [
-        'tabs',
-        'browsingData',
-        // etc...
-    ];
 
     try {
         files = await readDirAsync(inputFolder)
-        for (let file of files) {
+        for (let extension of files) {
 
             // clean up temp
             await rimraf(temp)
 
             // extract
-            console.log(file)
-            await extract(path.join(inputFolder, file), temp)
+            console.log('\n\n\nProcessing extension: ' + extension)
+            await extract(path.join(inputFolder, extension), temp)
 
             let actual = new Set()
             let declared = new Set()
+            let parserErrors = 0
 
             // collect applicable JS sources
             fromDir(temp, /.*js$/g, (file) => {
 
                 console.log('Checking file: ' + file)
                 contents = fs.readFileSync(file).toString()
-                
-                let ast = espree.parse(contents, {
-                    // attach range information to each node
-                    range: true,
-                    // attach line/column location information to each node
-                    loc: true,
-                    ecmaVersion: 7,
-                })
+                let ast = null
+                try {
+                    ast = espree.parse(contents, {
+                        // attach range information to each node
+                        range: true,
+                        // attach line/column location information to each node
+                        loc: true,
+                        ecmaVersion: 7,
+                    })
+                } catch (ex) {
+                    console.log('Error during analysis, continuing...', ex)
+                    parserErrors++
+                    return
+                }
 
-                // console.log(ast)
-
-
+                // traverse the ast, looking for clues of actual usages of permissions
                 estraverse.traverse(ast, {
                     enter: (node, parent) => {
-                        // console.log(node)
+                        
+                        // crude check... multiple depths of accesses e.g., chrome.system.cpu are not handled yet
+
                         if (
                             node.type === 'MemberExpression' &&
                             node.object && node.object.type === 'Identifier' &&
@@ -87,16 +151,44 @@ async function main() {
                     }
                 })
 
-                console.log(actual)
-                throw 1
             })
 
+            const manifest = JSON.parse(fs.readFileSync(path.join(temp, 'manifest.json')).toString())
+            for (let p of (manifest.permissions || [])) {
+
+                // filter host permissions for now 
+                // as they would always show up as unused
+                if (p.match && !p.match(/http(s)?\:\/\//)) {
+                    declared.add(p)
+                }
+            }
+
+            // compare and plot 
+            console.log(actual)
+            console.log(declared)
+
+            let union = new Set([...actual, ...declared])
+            let intersection = new Set([...actual].filter(elem => declared.has(elem)))
+            let undeclared = new Set([...actual].filter(elem => !declared.has(elem)))
+            let unused = new Set([...declared].filter(elem => !actual.has(elem))) 
+
+            console.log('union', union)
+            console.log('intersection', intersection)
+            console.log('undeclared', undeclared)
+            console.log('unused', unused)
+
+            result += extension + ';' +
+                [actual.size, declared.size, union.size, intersection.size, undeclared.size, unused.size].join(';') +
+                ';' + parserErrors + '\n'
+
+            // this is just a temp hack to get results even between extensions
+            fs.writeFileSync('results.csv', result);
         }
 
-        // declaredAPIs = permissions(manifestFile)
+        console.log("I can't believe it... It's actually done.")
 
-        // compare and plot 
 
+        fs.writeFileSync('results.csv', result);
 
     } catch (ex) {
         console.log(ex)
